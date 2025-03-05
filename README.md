544e2ffde984ce96ed8bbc319d51d5431d7a7c1d
fanghsiu <fanghsiu@outlook.com>
2025-03-05 14:07:38 +0800

    Update content

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Image Hosting
+https://cdn.fanghsiu.top
--- push.sh
+++ push.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# 如果没有消息后缀，默认提交信息为 :pencil: update content
-info=$1
-if ["$info" = ""];
-#then info="update time: $(date "+%Y-%m-%d %H:%M:%S")"
-then info="Initial commit"
-fi
+# 如果没有消息后缀，默认提交信息为 :pencil: Update content
+info=${1:-"Update content"}
 git add -A
 git commit -m "$info"
 git push
